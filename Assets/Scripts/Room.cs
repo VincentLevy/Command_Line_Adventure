7bc6634fc6b06a68f58173221f0b13dfa6d62df3
Vincent Levy <vicentelevy.a@gmail.com>
2021-01-30 15:43:03 -0700

    added basic input support

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+    public Room UpRoom;
+    public Room DownRoom;
+    public Room RightRoom;
+    public Room LeftRoom;
+
+    public List<string> ObjectList;
+
+    public bool HasRoomInDirection(string direction)
+    {
+        if (UpRoom == null && DownRoom == null && LeftRoom == null && RightRoom == null)
+            return false;
+
+        if(direction == "up" && UpRoom != null)
+        {
+            return true;
+        }
+
+        switch (direction)
+        {
+            case "up":
+
+                if (UpRoom != null)
+                    return true;
+
+                break;
+
+            case "down":
+
+                if (DownRoom != null)
+                    return true;
+
+                break;
+
+            case "right":
+
+                if (RightRoom != null)
+                    return true;
+
+                break;
+
+            case "left":
+
+                if (LeftRoom != null)
+                    return true;
+
+                break;
+        }
+
+        return false;
+    }
+}
--- Assets/Scripts/TextHandler.cs
+++ Assets/Scripts/TextHandler.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
 
 public class TextHandler : MonoBehaviour
 {
-    //[SerializeField]
-    //private TextMeshProUGUI InputText;
 
     [SerializeField]
     private TextMeshProUGUI HistoryText;
-
     [SerializeField]
     private TMP_InputField InputText;
-
     [SerializeField]
     private Scrollbar Scrollbar;
+    [SerializeField]
+    private EventSystem EventSystem;
+    [SerializeField]
+    private List<string> CommandList;
 
     public float offset = -1;
 
@@ -31,13 +33,83 @@ public class TextHandler : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            HistoryText.text += "C:\\Users\\Bonzo>" + InputText.text + "\n";
+            string LastCommand = InputText.text;
+            PrintMessage(LastCommand);
             InputText.text = "";
             Scrollbar.value = offset;
 
-           // InputField.
+            if(LastCommand != "")
+                ProcessLastCommand(LastCommand.ToLower());
         }
 
+        //constantly selects the input field
+        EventSystem.current.SetSelectedGameObject(InputText.gameObject, null);
+        InputText.OnPointerClick(new PointerEventData(EventSystem.current));
+    }
 
+    private void ProcessLastCommand(string lastCommand)
+    {
+        //looks bad I know
+        char[] charrarray = { ' ' };
+        string[] ParcedCommand = lastCommand.Split(charrarray);
+
+        if (ParcedCommand.Length != 0 && ParcedCommand[0] == "help")
+        {
+            DisplayHelpMessage();
+            return;
+        }
+
+        if (ParcedCommand.Length != 2)
+        {
+            PrintMessage("incorrect use of command");
+            return;
+        }
+
+        string CurrentCommand = ParcedCommand[0];
+        string CurrentArgument = ParcedCommand[1];
+
+        if (CommandList.Contains(ParcedCommand[0]))
+        {
+            switch (CurrentCommand)
+            {
+                case "help":
+
+                    break;
+
+                case "go":
+
+                    break;
+
+                case "talk":
+
+                    break;
+
+                case "download":
+
+                    break;
+
+                case "execute":
+
+                    break;
+
+                case "objects":
+
+                    break;
+            }
+        }
+        else
+        {
+            PrintMessage("there is no such command");
+        }
+    }
+
+    private void DisplayHelpMessage()
+    {
+        PrintMessage("Fuck you");
+    }
+
+    private void PrintMessage(string message)
+    {
+        HistoryText.text += "C:\\Users\\Bonzo>" + message + "\n";
     }
 }
