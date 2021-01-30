30990d3f7bf920d563ce3e5fb9951333e0adb754
Vincent Levy <vicentelevy.a@gmail.com>
2021-01-30 12:23:11 -0700

    First Commit; got a basic terminal thing going on

---
+++ Assets/Scripts/TextHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextHandler : MonoBehaviour
+{
+    //[SerializeField]
+    //private TextMeshProUGUI InputText;
+
+    [SerializeField]
+    private TextMeshProUGUI HistoryText;
+
+    [SerializeField]
+    private TMP_InputField InputText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputText.text = "hello world";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            HistoryText.text += "C:\\Users\\Bonzo>" + InputText.text + "\n";
+            InputText.text = "";
+        }
+    }
+}
